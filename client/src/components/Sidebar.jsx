7ee8d4c778a01df49021e54dd2800b2348490964
Muhammad Usman <usmanchaudhary112579@gmail.com>
2025-11-09 17:58:07 +0500

    Sidebar code updated

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -7,6 +7,9 @@ const Sidebar = () => {
         <div className='pb-5'>
           <div className='flex justify-center items-center'>
             <img src={assets.logo} alt="logo" className='max-w-40' />
+            <div className='relative py-2 group'>
+              <img src={assets.menu_icon} alt="menu" className='max-h-5 cursor-pointer' />
+            </div>
           </div>
         </div>
     </div>
