d880897f812413f341e003c8d18b2dcde4039fd0
Muhammad Usman <usmanchaudhary112579@gmail.com>
2025-11-09 15:14:21 +0500

    Added Sidebar and Home page logic to set it up

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import assets from '../assets/assets'
 
 const Sidebar = () => {
   return (
     <div>
-        <h1>Sidebar</h1>
+        <div className='pb-5'>
+          <div className='flex justify-center items-center'>
+            <img src={assets.logo} alt="logo" className='max-w-40' />
+          </div>
+        </div>
     </div>
   )
 }
