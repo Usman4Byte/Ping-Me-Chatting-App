bbb1af247a8e6366a0333252c6f052a313d439ab
Muhammad Usman <usmanchaudhary112579@gmail.com>
2025-11-09 14:45:26 +0500

    Day 2
    Added Assets
    Created Companents , pages, and added Tailwind

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import HomePage from './pages/HomePage'
+import LoginPage from './pages/LoginPage'
+import ProfilePage from './pages/ProfilePage'
 
 const App = () => {
   return (
-    <div>
-      
+    <div className="bg-[url('/src/assets/bgImage.png')] bg-cover bg-no-repeat bg-center">
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+      </Routes>
     </div>
   )
 }
