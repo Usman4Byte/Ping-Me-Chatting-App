5832e2c2427d1953467d3139cfd99740510cf932
Muhammad Usman <usmanchaudhary112579@gmail.com>
2025-11-10 21:59:45 +0500

    Created Users portion

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import assets from '../assets/assets'
+import assets, { userDummyData } from '../assets/assets'
 import { useNavigate } from 'react-router-dom'
 
 const Sidebar = ({selectedUser, setSelectedUser}) => {
@@ -19,10 +19,30 @@ const Sidebar = ({selectedUser, setSelectedUser}) => {
             </div>
           </div>
 
-          <div>
-            <img src={assets.search_icon} alt="search" className='w-3'/>
-            <input type="text" placeholder='Search or start new chat' className='bg-transparent border-none outline-none text-white text-xs placeholder-[#c8c8c8] flex-1'/>
+          <div className='bg-[#282142] rounded-full flex item-center gap-4 py-3 px-4 mt-5'>
+            <img src={assets.search_icon} alt="search" className='w-3 h-3'/>
+            <input type="text" placeholder='Search Chat...' className='bg-transparent border-none outline-none text-white text-xs placeholder-[#c8c8c8] flex-1'/>
           </div>
+
+        </div>
+
+        <div className='flex flex-col'>
+          {userDummyData.map((user, index)=>(
+            <div onClick={()=> {selectedUser(user)}} key={index} className={`reletive flex items-center gap-2 p-2 pl-4 rounded cursor-pointer max-sm:text-sm ${selectedUser?._id == user._id && 'bg-[#282142]/50'}`}>
+              <img src={user?.profilePic || assets.avatar_icon} alt="" className='w-[35px] aspect-[1/1] rounded-full'/>
+              <div className='flex flex-col leading-5'>
+                <p>{user.fullName}</p>
+                {
+                  index < 3
+                  ? <span className='text-green-400 text-xs'>Online</span>
+                  : <span className='text-xs text-neutral-400'>Offline</span>
+                }
+              </div>
+
+              {index > 2 && <p className='absolute top-4 right-4 text-xs h-5 w-5 flex justify-center items-center rounded-full bg-violet-500/50'>{index}</p>}
+
+            </div>
+          ))}
         </div>
     </div>
   )
