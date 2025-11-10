e57abb9f8982922f77be9d009f36b1b47bbaa805
Muhammad Usman <usmanchaudhary112579@gmail.com>
2025-11-10 22:33:44 +0500

    Sidebar completed
    Frontend of Sidebar along-with collapsing is completed, rest will do later (10/11)

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -28,7 +28,7 @@ const Sidebar = ({selectedUser, setSelectedUser}) => {
 
         <div className='flex flex-col'>
           {userDummyData.map((user, index)=>(
-            <div onClick={()=> {selectedUser(user)}} key={index} className={`reletive flex items-center gap-2 p-2 pl-4 rounded cursor-pointer max-sm:text-sm ${selectedUser?._id == user._id && 'bg-[#282142]/50'}`}>
+            <div onClick={()=> {setSelectedUser(user)}} key={index} className={`relative flex items-center gap-2 p-2 pl-4 rounded-full cursor-pointer max-sm:text-sm ${selectedUser?._id == user._id && 'bg-[#282142]/50'}`}>
               <img src={user?.profilePic || assets.avatar_icon} alt="" className='w-[35px] aspect-[1/1] rounded-full'/>
               <div className='flex flex-col leading-5'>
                 <p>{user.fullName}</p>
@@ -39,7 +39,7 @@ const Sidebar = ({selectedUser, setSelectedUser}) => {
                 }
               </div>
 
-              {index > 2 && <p className='absolute top-4 right-4 text-xs h-5 w-5 flex justify-center items-center rounded-full bg-violet-500/50'>{index}</p>}
+              {index > 2 && <p className='absolute top-4 right-4 text-xs h-5 w-5 flex justify-center items-center rounded-full bg-violet-500 text-white z-10'>{index}</p>}
 
             </div>
           ))}
