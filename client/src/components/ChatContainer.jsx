f007fa434ac32efd1f3d0ff3aebe7024bf15b625
Muhammad Usman <usmanchaudhary112579@gmail.com>
2025-11-12 21:18:57 +0500

    Chat container completed

--- client/src/components/ChatContainer.jsx
+++ client/src/components/ChatContainer.jsx
@@ -48,10 +48,14 @@ const ChatContainer = ({selectedUser, setSelectedUser}) => {
       {/* bottom area */}
       
       <div className='absolute bottom-0 left-0 right-0 flex items-center gap-3 p-3'>
-          <div>
-            <input type="text" placeholder='Send a message...'/>
-            <input type="file" id='image' accept='image/png, image/jpg, image/jpeg, image/webp'/>
+          <div className='flex-1 flex items-center bg-gray-100/12 px-3 rounded-full'>
+            <input type="text" placeholder='Send a message...' className='flex-1 text-sm p-3 border-none rounded-lg outline-none text-white placeholder-gray-400'/>
+            <input type="file" id='image' accept='image/png, image/jpg, image/jpeg, image/webp' hidden/>
+            <label htmlFor="image">
+              <img src={assets.gallery_icon} alt="" className='w-5 mr-2 cursor-pointer'/>
+            </label>
           </div>
+          <img src={assets.send_button} alt="" className='cursor-pointer w-7'/>
       </div>
 
     </div>
