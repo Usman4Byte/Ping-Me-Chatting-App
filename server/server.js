deebc6e588bc89ff59d5832926506642ee60d6de
Muhammad Usman <usmanchaudhary112579@gmail.com>
2025-11-16 18:23:56 +0500

    Added cloudinary, utils, auth, and routes

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ import express from "express";
 import "dotenv/config";
 import cors from "cors";
 import http from "http";
+import { connectDB } from "./lib/db.js";
+import userRouter from "./routes/userRoutes.js";
 
 // Create Express app and HTTP server
 const app = express();
@@ -12,6 +14,10 @@ app.use(express.json({limit: "4mb"}));
 app.use(cors());
 
 app.use("/api/status", (req, res) => res.send("Server is live"));
+app.use("/api/auth", userRouter);
+
+// Connect to the database
+await connectDB();
 
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
