1f46d44c998c6c2771fdd0967c38cb27027d1ecb
Muhammad Usman <usmanchaudhary112579@gmail.com>
2025-11-19 23:22:12 +0500

    Chat container completed and working fine

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,11 +1,16 @@
 import React, { useState, useRef, useEffect, useContext } from 'react'
-import assets, { userDummyData } from '../assets/assets'
+import assets from '../assets/assets'
 import { useNavigate } from 'react-router-dom'
 import { AuthContext } from '../../context/AuthContext.jsx'
+import { ChatContext } from '../../context/ChatContext.jsx'
 
-const Sidebar = ({selectedUser, setSelectedUser}) => {
+const Sidebar = () => {
 
-  const {logout} = useContext(AuthContext);
+  const { getUsers, users, selectedUser, setSelectedUser, unseenMessages, setUnseenMessages } = useContext(ChatContext);
+
+  const { logout, onlineUsers } = useContext(AuthContext);
+
+  const [input, setInput] = useState(false);
 
   const navigate = useNavigate();
   const [menuOpen, setMenuOpen] = useState(false)
@@ -20,53 +25,60 @@ const Sidebar = ({selectedUser, setSelectedUser}) => {
     document.addEventListener('click', handleDocClick)
     return () => document.removeEventListener('click', handleDocClick)
   }, [])
+
+  const filteredUsers = input ? users.filter((user) => user.fullName.toLowerCase().includes(input.toLowerCase())) : users;
+
+  useEffect(() => {
+    getUsers();
+  }, [onlineUsers])
+  
   return (
     <div className={`bg-[#8185B2]/10 h-full p-5 rounded-r-xl overflow-y-auto text-white ${selectedUser ? "max-md:hidden" : ''}`}>
-        <div className='pb-5'>
-          <div className='flex justify-between items-center'>
-            <img src={assets.logo} alt="logo" className='max-w-40' />
-            <div ref={menuRef} className='relative py-2'>
-              <img
-                src={assets.menu_icon}
-                alt="menu"
-                className='max-h-5 cursor-pointer'
-                onClick={(e) => { e.stopPropagation(); setMenuOpen(prev => !prev) }}
-              />
-              <div
-                onClick={(e) => e.stopPropagation()}
-                className={`absolute top-full right-0 z-20 w-32 p-5 rounded-md bg-[#282142] border border-gray-600 text-100 ${menuOpen ? 'block' : 'hidden'}`}>
-                <p onClick={() => { setMenuOpen(false); navigate('/profile') }} className='cursor-pointer text-sm'>Edit Profile</p>
-                <hr className='my-2 border-t border-gray-500'/>
-                <p onClick={() => { setMenuOpen(false); logout();}} className='cursor-pointer text-sm'>Logout</p>
-              </div>
+      <div className='pb-5'>
+        <div className='flex justify-between items-center'>
+          <img src={assets.logo} alt="logo" className='max-w-40' />
+          <div ref={menuRef} className='relative py-2'>
+            <img
+              src={assets.menu_icon}
+              alt="menu"
+              className='max-h-5 cursor-pointer'
+              onClick={(e) => { e.stopPropagation(); setMenuOpen(prev => !prev) }}
+            />
+            <div
+              onClick={(e) => e.stopPropagation()}
+              className={`absolute top-full right-0 z-20 w-32 p-5 rounded-md bg-[#282142] border border-gray-600 text-100 ${menuOpen ? 'block' : 'hidden'}`}>
+              <p onClick={() => { setMenuOpen(false); navigate('/profile') }} className='cursor-pointer text-sm'>Edit Profile</p>
+              <hr className='my-2 border-t border-gray-500' />
+              <p onClick={() => { setMenuOpen(false); logout(); }} className='cursor-pointer text-sm'>Logout</p>
             </div>
           </div>
-
-          <div className='bg-[#282142] rounded-full flex item-center gap-4 py-3 px-4 mt-5'>
-            <img src={assets.search_icon} alt="search" className='w-3 h-3'/>
-            <input type="text" placeholder='Search Chat...' className='bg-transparent border-none outline-none text-white text-xs placeholder-[#c8c8c8] flex-1'/>
-          </div>
-
         </div>
 
-        <div className='flex flex-col'>
-          {userDummyData.map((user, index)=>(
-            <div onClick={()=> {setSelectedUser(user)}} key={index} className={`relative flex items-center gap-2 p-2 pl-4 rounded-full cursor-pointer max-sm:text-sm ${selectedUser?._id == user._id && 'bg-[#282142]/50'}`}>
-              <img src={user?.profilePic || assets.avatar_icon} alt="" className='w-[35px] aspect-[1/1] rounded-full'/>
-              <div className='flex flex-col leading-5'>
-                <p>{user.fullName}</p>
-                {
-                  index < 3
+        <div className='bg-[#282142] rounded-full flex item-center gap-4 py-3 px-4 mt-5'>
+          <img src={assets.search_icon} alt="search" className='w-3 h-3' />
+          <input onChange={(e) => setInput(e.target.value)} type="text" placeholder='Search Chat...' className='bg-transparent border-none outline-none text-white text-xs placeholder-[#c8c8c8] flex-1' />
+        </div>
+
+      </div>
+
+      <div className='flex flex-col'>
+        {filteredUsers.map((user, index) => (
+          <div onClick={() => { setSelectedUser(user); setUnseenMessages(prev => ({ ...prev, [user._id]: 0 })) }} key={index} className={`relative flex items-center gap-2 p-2 pl-4 rounded-full cursor-pointer max-sm:text-sm ${selectedUser?._id == user._id && 'bg-[#282142]/50'}`}>
+            <img src={user?.profilePic || assets.avatar_icon} alt="" className='w-[35px] aspect-[1/1] rounded-full' />
+            <div className='flex flex-col leading-5'>
+              <p>{user.fullName}</p>
+              {
+                onlineUsers.includes(user._id)
                   ? <span className='text-green-400 text-xs'>Online</span>
                   : <span className='text-xs text-neutral-400'>Offline</span>
-                }
-              </div>
-
-              {index > 2 && <p className='absolute top-4 right-4 text-xs h-5 w-5 flex justify-center items-center rounded-full bg-violet-500 text-white z-10'>{index}</p>}
-
+              }
             </div>
-          ))}
-        </div>
+
+            {unseenMessages[user._id] > 0 && <p className='absolute top-4 right-4 text-xs h-5 w-5 flex justify-center items-center rounded-full bg-violet-500 text-white z-10'>{unseenMessages[user._id]}</p>}
+
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
